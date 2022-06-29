5b1c53ba9b798d79eae474529c53475e49d31576
Souata Majumdar <majumdar.deba@gmail.com>
2022-06-29 12:06:47 +0530

    Committing jpa changes

---
+++ src/main/java/com/example/jpa/App.java
@@ -0,0 +1,32 @@
+package com.example.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        System.out.println( "Hello World!" );
+        
+        Employee employee = new Employee();
+        employee.setFirstName("Dalia");
+        employee.setLastName("Abo Sheasha");
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(employee);
+        entityManager.getTransaction().commit();
+        
+        
+        entityManager.close();
+    
+        entityManagerFactory.close();
+    }
+}
